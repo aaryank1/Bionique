436e218286be311a7598688bab71c8df82fc2377
Aaryan <aaryankakade1@gmail.com>
2024-07-31 08:35:57 +0530

    logic for text conversion

--- Frontend/src/components/MainPage.jsx
+++ Frontend/src/components/MainPage.jsx
@@ -6,26 +6,44 @@ const MainPage = () => {
 
   const textareaRef = useRef(null);
   const [value, setValue] = useState('');
+
+  const [inputText, setInputText] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  }
   
   useEffect(() => {
-    if(textareaRef.current){
-      textareaRef.current.style.height = 'auto';
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-    }
+    textareaRef.current.style.height = 'auto';
+    textareaRef.current.style.height = textareaRef.current.scrollHeight+'px';
   },[value]);
+
+  const convert = (e) => {
+    let input = e.target.value;
+    return input.split(' ').map(word => {
+      const midPoint = Math.ceil(word.length/2);
+      const firstHalf = word.substring(0, midPoint);
+      const secondHalf = word.substring(midPoint);
+
+      return `<b>${firstHalf}</b>${secondHalf}`;
+    }).join(' ');
+  }
 
   return (
     <div className='mainpage'>
         <Navbar />
-        <div className="container">
+        <div className="mainpage_container">
 
-          <div className="converter">
+          <div className="convertor">
             
             <h1 className='convertor_title'>Bionic Reading</h1>
-            <label htmlFor="textarea">Paste Your Text</label>
-            <textarea ref={textareaRef} name="textarea" id="textarea" placeholder='Enter Your text here'></textarea>
-            <div className="result">
-              <p className='resultant'></p>
+            <div className="bionic_convertor">
+              <label className='label' htmlFor="textarea">Paste Your Text</label>
+              <textarea onChange={handleChange} ref={textareaRef} name="textarea" id="textarea" placeholder='Enter Your text here'></textarea>
+              <button onClick={convert} className='submit' >Convert</button>
+              <div className="result">
+                <p className='resultant'></p>
+              </div>
             </div>
           </div>
           
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../styles/navbar.css'
 import {assets} from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
   return (
     <div className='navbar'>
-        <img className='logo' src={assets.bionique_transparent} alt="logo" />
+        <Link to={'/bionique'}>
+          <img className='logo' src={assets.bionique_transparent} alt="logo" />
+        </Link>
         <div className="user_opts">
             <button className='signin' >SignIn</button>
         </div>
